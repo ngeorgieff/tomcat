516c2ca9473feeff4cd1c87c81d0a6efa46b6de9
Nikolay Georgieff <ngeorgieff@it.ucla.edu>
2014-08-06 14:18:53 -0700

    changed user shell

--- manifests/init.pp
+++ manifests/init.pp
@@ -76,7 +76,7 @@ user { $tomcat_user:
     password         => '!!',
     password_max_age => '99999',
     password_min_age => '0',
-    shell            => '/sbin/nologin',
+    shell            => '/bin/bash',
     uid              => '8080',
 }
 group { $tomcat_user:
